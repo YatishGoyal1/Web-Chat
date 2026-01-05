f84154649bf111f478018259ca38f7968212ebc1
Yatish Goyal <yatishg008@gmail.com>
2026-01-05 17:12:21 +0530

    just

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def home():
         elif code not in rooms:
             return render_template("home.html",error ="Room does not exist.",code = code,name = name)
         session["room"]= room
-        session["name"]=name
+        session["name"]= name
         return redirect(url_for("room"))
     return render_template('home.html')
 
@@ -45,6 +45,32 @@ def room():
     if room is None or session.get('room') is None or room  not in rooms:
         return redirect(url_for("home"))
     return render_template("room.html")
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    join_room(room)
+    send({"name": name,"message":"has entered the room"},to=room)
+    rooms[room]["members"] +=1
+    print(f"{name} joined room {room}")
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -=1
+        if rooms[room]["members"] <=0:
+            del rooms[room]
+    send({"name": name,"message":"has left the room"},to=room)
+    print(f"{name} has left the room {room}")
+
 
 if __name__ == "__main__":
     socketio.run(app,debug=True)
