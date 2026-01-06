97de90bf6a5e0b358fb76b104dbcc581ab32280d
Yatish Goyal <yatishg008@gmail.com>
2026-01-06 14:17:04 +0530

    commit

--- app.py
+++ app.py
@@ -62,26 +62,30 @@ def connect(auth):
     room = session.get("room")
     name = session.get("name")
 
-    if not room or not name:
+    if not room or not name or room not in rooms:
         return
-    if room not in rooms:
-        leave_room(room)
-        return
+
     join_room(room)
-    send({"name": name,"message":"has entered the room"},to=room)
-    rooms[room]["members"] +=1
-    print(f"{name} joined room {room}")
+
+    if rooms[room]["members"] == 0:
+        send({"name": name, "message": "has entered the room"}, to=room)
+
+    rooms[room]["members"] += 1
+
 @socketio.on("disconnect")
 def disconnect():
     room = session.get("room")
     name = session.get("name")
+
+    if not room or room not in rooms:
+        return
+
     leave_room(room)
-    if room in rooms:
-        rooms[room]["members"] -=1
-        if rooms[room]["members"] <=0:
-            del rooms[room]
-    send({"name": name,"message":"has left the room"},to=room)
-    print(f"{name} has left the room {room}")
+    rooms[room]["members"] -= 1
+    send({"name": name, "message": "has left the room"}, to=room)
+    if rooms[room]["members"] <= 0:
+        del rooms[room]
+
 
 
 if __name__ == "__main__":
