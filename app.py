def15517880b7fe2aa871beca7050e7101992497
Yatish Goyal <yatishg008@gmail.com>
2026-01-06 14:41:01 +0530

    i am tired

--- app.py
+++ app.py
@@ -65,13 +65,11 @@ def connect(auth):
     if not room or not name:
         return
     if room not in rooms:
-        leave_room(room)
         return
     join_room(room)
-    if join_room(room) != False:
-       send({"name": name,"message":"has entered the room"},to=room)
-       rooms[room]["members"] +=1
-       print(f"{name} joined room {room}")
+    send({"name": name,"message":"has entered the room"},to=room)
+    rooms[room]["members"] +=1
+    print(f"{name} joined room {room}")
 @socketio.on("disconnect")
 def disconnect():
     room = session.get("room")
@@ -79,8 +77,7 @@ def disconnect():
     leave_room(room)
     if room in rooms:
         rooms[room]["members"] -=1
-        if rooms[room]["members"] <=0:
-            del rooms[room]
+
     send({"name": name,"message":"has left the room"},to=room)
     print(f"{name} has left the room {room}")
 
